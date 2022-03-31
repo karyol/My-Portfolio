cc698250dd6927f12590d763b65ec1531f842494
karyol <karyol.kj@gmail.com>
2022-03-31 16:07:53 +0200

    Update tradeAway.js

--- src/pages/tradeAway.js
+++ src/pages/tradeAway.js
@@ -1,24 +1,6 @@
 import React, { Suspense } from 'react'
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
 
 const TradeAwayComp = React.lazy(() => import('../components/tradeAwayComp'));
-
-i18n
-  .use(initReactI18next)
-  .use(LanguageDetector)
-  .use(HttpApi)
-  .init({
-    supportedLngs: ['en', 'pl'],
-    fallbackLng: 'en',
-    detection: {
-        order: ['cookie', 'localStorage', 'htmlTag', 'path', 'subdomain'],
-        caches: ['cookie'],
-    },
-    backend: {
-        loadPath: '/assets/locales/{{lng}}/translation.json',
-    },
-  });
 
 const loadingMarkup = (
     <div style={{ 
